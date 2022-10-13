4038a5c1ccb579cb2738909fa942b3384a535bf9
Bernard Legaspi <blegaspi26@gmail.com>
2022-10-13 10:35:36 -0400

    commit tree cfeb89b60cbae6e7383ece9407dce46f57e8567a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Bernard",
+    streetAddress: {
+        city: "Jersey City",
+        state: "New Jersey"
+    }
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    //spread operator copies all the elements of the existing array into the new one 
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return employee, newEmployee;
+}
+
+// this function updates employee; key & value and returns the updated employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+//deletes key from a clone of employee and returns the new employee 
+function deleteFromEmployeeByKey(employee, key) {
+
+    var newEmployee = { ...employee }
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+//works the same as the previous function, should mutate employee object 
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee;
+}
+
